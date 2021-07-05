a61b93588087d076b74c87db31aadc2bbab170b6
Steven van Rosendaal <svanrosendaal@hotmail.com>
2021-07-05 13:57:53 +0200

    improved .env.example file, and added a required checkbox for going back to the search products page

--- resources/views/components/search-products-modal.blade.php
+++ resources/views/components/search-products-modal.blade.php
@@ -9,11 +9,30 @@
             </div>
             <div class="modal-body">
                 <p>Als je naar de "Producten Uitlenen" pagina gaat, wordt je uitgelogd. Weet je het zeker?</p>
+                <p class="text-danger">LET OP: Je wordt uitgelogd op deze app, maar NIET op de Curio Codes. Als dit systeem gebruikt wordt om producten op uit te lenen, is het aan te raden om ook op de Curio Codes uit te loggen, anders worden gebruikers automatisch ingelogd op het openstaande account als er op inloggen geklikt wordt.</p>
+                <div class="d-flex">
+                    <label class="mt-1 mr-2 font-weight-bold" for="logoutCheck">Ik ben uitgelogd op Curio Codes, of ik besef de risico's:</label>
+                    <input id="logoutCheck" style="width: 30px; height: 30px;" name="logoutCheck" type="checkbox">
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuleren</button>
-                <a href="{{ route('searchProducts') }}" class="btn btn-danger">Uitloggen en Doorgaan</a>
+                <a id="logoutButton" href="{{ route('searchProducts') }}" class="btn btn-danger disabled">Uitloggen en Doorgaan</a>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+    document.getElementById('logoutCheck').addEventListener("click", checkProductCheck);
+
+    function checkProductCheck() {
+        if(document.getElementById('logoutCheck').checked === true) {
+            document.getElementById('logoutButton').classList.remove('disabled');
+        } else {
+            document.getElementById('logoutButton').classList.add('disabled');
+        }
+    }
+
+</script>
