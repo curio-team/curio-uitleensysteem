e865451a0bd615e9da527d50c96d6bfe031f4b67
Steven van Rosendaal <svanrosendaal@hotmail.com>
2021-07-07 17:36:42 +0200

    fixed a logout bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('reserveProduct') }}">
+                                    <a class="dropdown-item" href="{{ route('searchProducts') }}">
                                         {{ __('Logout') }}
                                     </a>
                                 </div>
